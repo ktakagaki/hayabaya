99b0f104f4527d19b15630bd0072b8b6fcb8b49c
Soren Lentz <slentzen@gmail.com>
2015-04-19 14:41:56 +0200

    Remember to change JAVA_HOME Updated build.gradle to comply with java 8

--- src/main/java/org/hayabaya/Hayabaya.java
+++ src/main/java/org/hayabaya/Hayabaya.java
@@ -13,7 +13,7 @@ public class Hayabaya {
     //ToDo: Implement scaling factors so any stepsize can be used and calculation of numberOfX scales accordingly
     //ToDo 2: you might want to encapsulate all of the following in a class or a static thing
     static final public int ARRAY_SIZE_MIN =    1_000; // thousand
-    static final public int ARRAY_SIZE_MAX =  100_000; // hundred thousand
+    static final public int ARRAY_SIZE_MAX =  10_000; // hundred thousand
     static final public int ARRAY_SIZE_STEPS =  1_000;
 
     static final public int CYCLES_MIN   = 1_000; // thousand
