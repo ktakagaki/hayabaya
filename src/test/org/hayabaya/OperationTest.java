093e865fdd682d50f1531cb7c1f68acfb50d0981
Soren Lentz <slentzen@gmail.com>
2015-04-21 13:14:07 +0200

    Set /test as Test Sources Root

---
+++ src/test/org/hayabaya/OperationTest.java
@@ -0,0 +1,26 @@
+package org.hayabaya;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cain on 4/21/2015.
+ */
+public class OperationTest {
+    
+    @Test
+    public void testName() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+}
