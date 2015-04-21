42944e2588436850f90c88a948ae2f2c73ab05c4
Soren Lentz <slentzen@gmail.com>
2015-04-21 19:30:54 +0200

    Added test for IllegalArgumentException

--- src/test/org/hayabaya/OperationTest.java
+++ src/test/org/hayabaya/OperationTest.java
@@ -12,7 +12,14 @@ public class OperationTest {
 
     @Test
     public void testToString() throws Exception {
+        assertThat("Add",       equalTo(Operation.ADD.toString()));
+        assertThat("Subtract",  equalTo(Operation.SUBTRACT.toString()));
+        assertThat("Multiply",  equalTo(Operation.MULTIPLY.toString()));
+        assertThat("Divide",    equalTo(Operation.DIVIDE.toString()));
+    }
 
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testThrowsIllegalArgumentException() {
+        Operation myOperation = Enum.valueOf(Operation.class, "Proton");
     }
 }
