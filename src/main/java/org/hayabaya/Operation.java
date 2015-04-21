4b7a1d5b02ae92222dd7ddc38e28920852cebae9
Soren Lentz <slentzen@gmail.com>
2015-04-21 19:23:37 +0200

    Enum toSTring method added plus tests

--- src/main/java/org/hayabaya/Operation.java
+++ src/main/java/org/hayabaya/Operation.java
@@ -4,5 +4,26 @@ package org.hayabaya;
  * Created by ktakagaki on 15/04/08.
  */
 public enum Operation {
-    ADD, SUBTRACT, MULTIPLY, DIVIDE
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case ADD:
+                return "Add";
+            case SUBTRACT:
+                return "Subtract";
+            case MULTIPLY:
+                return "Multiply";
+            case DIVIDE:
+                return "Divide";
+            default:
+                throw new IllegalArgumentException();
+        }
+
+    }
+
 }
--- src/test/org/hayabaya/OperationTest.java
+++ src/test/org/hayabaya/OperationTest.java
@@ -1,26 +1,18 @@
 package org.hayabaya;
 
 import org.junit.Test;
-
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+
 
 /**
  * Created by cain on 4/21/2015.
  */
 public class OperationTest {
-    
-    @Test
-    public void testName() throws Exception {
-
-    }
 
     @Test
     public void testToString() throws Exception {
 
-    }
-
-    @Test
-    public void testEquals() throws Exception {
 
     }
 }
