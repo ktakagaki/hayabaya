d745ce7b7b0fb9d135bc067821816e7244eb7d9d
Soren Lentz <slentzen@gmail.com>
2015-04-21 19:22:33 +0200

    Enum Tpe (Type) now has a toString method

--- src/main/java/org/hayabaya/Tpe.java
+++ src/main/java/org/hayabaya/Tpe.java
@@ -4,5 +4,33 @@ package org.hayabaya;
  * Created by ktakagaki on 15/04/08.
  */
 public enum Tpe {
-    INT, LONG, DOUBLE, FLOAT, INTEGER_BOXED, LONG_BOXED, DOUBLE_BOXED, FLOAT_BOXED
+    INT,    INTEGER_BOXED,
+    FLOAT,  FLOAT_BOXED,
+    DOUBLE, DOUBLE_BOXED,
+    LONG,   LONG_BOXED;
+
+    @Override
+    public String toString() {
+        switch(this) {
+            case INT:
+                return "Integer";
+            case INTEGER_BOXED:
+                return "Integer_Boxed";
+            case FLOAT:
+                return "Float";
+            case FLOAT_BOXED:
+                return "Float_Boxed";
+            case DOUBLE:
+                return "Double";
+            case DOUBLE_BOXED:
+                return "Double_Boxed";
+            case LONG:
+                return "Long";
+            case LONG_BOXED:
+                return "Long_Boxed";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
 }
