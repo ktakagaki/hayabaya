a8471424e6297191817e762376f7c7d58c9ae130
Soren Lentz <slentzen@gmail.com>
2015-04-24 22:22:45 +0200

    Preparing to unit test autoboxed loopers

---
+++ src/test/org/hayabaya/loopers/LoopersIntegerBoxedTest.java
@@ -0,0 +1,68 @@
+package org.hayabaya.loopers;
+
+import org.hayabaya.MyTestAbstract;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cain on 4/24/2015.
+ */
+public class LoopersIntegerBoxedTest extends MyTestAbstract {
+    
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testOperateLoop() throws Exception {
+
+    }
+
+    @Test
+    public void testInitArray() throws Exception {
+
+    }
+
+    @Test
+    public void testOperateLoop1() throws Exception {
+
+    }
+
+    @Test
+    public void testInitArray1() throws Exception {
+
+    }
+
+    @Test
+    public void testSetArrayLength() throws Exception {
+
+    }
+
+    @Test
+    public void testGetArrayLength() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCycles() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCycles() throws Exception {
+
+    }
+
+    @Test
+    public void testTest1() throws Exception {
+
+    }
+
+    @Test
+    public void testMakeResults() throws Exception {
+
+    }
+}
