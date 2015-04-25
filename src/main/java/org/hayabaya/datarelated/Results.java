37c6d28c9bd6315fa068744ff4e25d0b8eb4bb0e
Soren Lentz <slentzen@gmail.com>
2015-04-25 18:29:45 +0200

    Added a toString() method

--- src/main/java/org/hayabaya/datarelated/Results.java
+++ src/main/java/org/hayabaya/datarelated/Results.java
@@ -1,5 +1,7 @@
 package org.hayabaya.datarelated;
 
+import java.util.Arrays;
+
 /**
  * Created by ktakagaki on 15/04/08.
  */
@@ -24,5 +26,31 @@ public class Results {
         this.type = type;
         this.operation = operation;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        String NEW_LINE = System.getProperty("line.separator");
+
+        // Name
+        result.append(Utility.ANSI_BLUE + "{Object:            "
+                + Utility.ANSI_RESET + this.getClass().getSimpleName()+ "}" + NEW_LINE);
+        // Cycles
+        result.append(Utility.ANSI_CYAN + "{Cycles:            " + Utility.ANSI_RESET +
+                cycles+ "}" + NEW_LINE);
+        // Type
+        result.append(Utility.ANSI_PURPLE + "{Type:              " + Utility.ANSI_RESET +
+                type+ "}" + NEW_LINE);
+        // Operation
+        result.append(Utility.ANSI_GREEN + "{Type:              " + Utility.ANSI_RESET +
+                type+ "}" + NEW_LINE);
+
+        result.append("The resulting Array contains the following elements: \n \n");
+
+        result.append(Arrays.deepToString(data));
+
+        return result.toString();
+    }
+
 
 }
