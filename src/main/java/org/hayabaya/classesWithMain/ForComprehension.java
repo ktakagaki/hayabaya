42eac03c74370d19a6c78e0a63f85cb0aa78ec4e
Soren Lentz <slentzen@gmail.com>
2015-05-26 13:21:04 +0200

    Added convertToPrimitive Array. Directly copied from: http://stackoverflow.com/questions/718554/how-to-convert-an-arraylist-containing-integers-to-primitive-int-array

--- src/main/java/org/hayabaya/classesWithMain/ForComprehension.java
+++ src/main/java/org/hayabaya/classesWithMain/ForComprehension.java
@@ -1,37 +1,57 @@
 package org.hayabaya.classesWithMain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.Math;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by cain on 5/26/2015.
  */
 public class ForComprehension {
     public static void main(String[] args) {
-        int myMin = 1;
-        int myMax = 100;
-        int steps = 10;
-        System.out.println("The result of generateIntegerLinspace(" +myMin+ ", " +myMax+ ", " +steps+ ")");
+        System.out.println("Calling f(10, 100, 10): ");
+        System.out.println(Arrays.toString(generateIntegerLinspace(10, 100, 10)));
 
-        int[] test = generateIntegerLinspace(myMin, myMax, steps);
+        System.out.println("Calling f(1, 10, 1): ");
+        System.out.println(Arrays.toString(generateIntegerLinspace(1, 10, 1)));
 
-        System.out.println(Arrays.toString(test));
+        System.out.println("Calling f(5, 50, 10): ");
+        System.out.println(Arrays.toString(generateIntegerLinspace(5, 50, 10)));
 
 
     }
-    public static int[] generateIntegerLinspace(int min, int max, int steps) {
+    public static int[] generateIntegerLinspace(int min, int max, int incrementSize) {
         assert min > 0 : "array length must be > 0";
         assert max > 0 : "array length must be > 0";
-        assert steps > 0 : "steps must be > 0";
+        assert max > min : " max must be > min";
+        assert incrementSize > 0 : "incrementSize must be > 0";
+        
+        int arrayLength = (int) Math.ceil(((max - min)) / incrementSize) + 1;
+        ArrayList<Integer> arl = new ArrayList<>(arrayLength);
 
-        int arrayLength = (int) Math.ceil(((max - min)) / steps) + 1;
-        int result[] = new int[arrayLength];
-        int dy = (max - min) / (steps -1);
+        arl.add(min);
+        int count = min;
 
-        for (int i = 0; i < steps; i++){
-            result[i] = min + (dy * i);
+        while (count < max) {
+            count = count + incrementSize;
+            arl.add(count);
         }
+        arl.add(max);
+
+        int[] result = convertToPrimitiveArray(arl);
 
         return result;
+    }
+    public static int[] convertToPrimitiveArray(List<Integer> integers)
+    {
+        int[] ret = new int[integers.size()];
+        Iterator<Integer> iterator = integers.iterator();
+        for (int i = 0; i < ret.length; i++)
+        {
+            ret[i] = iterator.next().intValue();
+        }
+        return ret;
     }
 }
