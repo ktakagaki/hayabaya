ef0ef75ba3468a7b61234190677be9ec11f4e8ff
Slentzen Demian <slentzen@gmail.com>
2015-09-21 23:13:18 +0200

    tpe annotated

--- src/main/java/org/hayabaya/datarelated/Tpe.java
+++ src/main/java/org/hayabaya/datarelated/Tpe.java
@@ -1,7 +1,17 @@
 package org.hayabaya.datarelated;
 
 /**
- * Created by ktakagaki on 15/04/08.
+ * The data types available in the Hayabaya project
+ * <ul>
+ *     <li><b>INT</b> Integer</li>
+ *     <li><b>INTEGER_BOXED</b> Boxed Integer</li>
+ *     <li><b>FLOAT</b> Float</li>
+ *     <li><b>FLOAT_BOXED</b> Boxed Float</li>
+ *     <li><b>DOUBLE</b> Double</li>
+ *     <li><b>DOUBLE_BOXED</b> Boxed Double</li>
+ *     <li><b>LONG</b> Long</li>
+ *     <li><b>LONG_BOXED</b> Boxed Long</li>
+ * </ul>
  */
 public enum Tpe {
     INT,
