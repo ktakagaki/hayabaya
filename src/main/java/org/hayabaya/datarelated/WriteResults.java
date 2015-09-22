19c699f6cd37a9a1bd91b79eaad655c4251abe88
Slentzen Demian <slentzen@gmail.com>
2015-09-22 22:45:48 +0200

    attempt to write to file only header

--- src/main/java/org/hayabaya/datarelated/WriteResults.java
+++ src/main/java/org/hayabaya/datarelated/WriteResults.java
@@ -68,104 +68,107 @@ public class WriteResults {
 
 
             String fileText = sb.toString();
-        } catch (Exception e) {
 
-        }
-
-
-
-
-        try {
             Path resultFilePath = Paths.get(outPutFolderPath + results.getFileName());
             File newTextFile = new File(resultFilePath.toString());
 
             FileWriter fw = new FileWriter(newTextFile);
             fw.write(fileText);
             fw.close();
-
-        } catch (Exception e) {
+        } catch (IOException e) {
+            logger.error("File error {}", e);
 
         }
 
-            // Create needed local data
 
 
-
-            // Create the results folder
-            String fileDir = nameCPU + "_results";
-            File fileDirObject = new File(fileDir);
-            //ToDo: Test if folder exits and ensure it works on windows AND linux
-            if (!fileDirObject.exists()) fileDirObject.mkdir();
-
-            // Create the file and open the connection
-            String filename = fileDir + "/" + nameCPU + "_res_" + dataType + "_" +
-                    operation + "_rep_" + repetitionNumber + ".csv";
-
-            // Open up the connection and create a string builder
-            BufferedWriter br = new BufferedWriter(new FileWriter(filename));
-            StringBuilder header = new StringBuilder();
-
-            // Create the header/first line
-            header.append("name,\t\t DataType,\t isBoxed,\t operation,\t Repetition,\t ArrayLength\t " +
-                    "CycleNumber,\t RunTime,\t");
-            br.write(header.toString());
-            br.newLine();
-
-            // Begin writing the actual data
-            int arrayLengthNumbers = runSettings.getArrayLengths().length;
-            int cycleLengthNumbers = runSettings.getCycleNumbers().length;
-            int rowNumbers = arrayLengthNumbers * cycleLengthNumbers;
-
-
-            //Start producing each line
-            int[] arrayLengths = runSettings.getArrayLengths();
-            int[] cycleNumbers = runSettings.getCycleNumbers();
-            int arrayLen = arrayLengths.length;
-            int cycleLen = cycleNumbers.length;
-
-            int rowIndex = 0, columnIndex = 0;
-
-            //StringBuilder rowLine = new StringBuilder();
-            //rowLine.append(resultLine);
-            // Missing values are ArrayLength CycleNumber and Runtime
-            String resultLine = nameCPU + ",\t   " + dataType + ",\t   " + isBoxed + ",\t\t" + operation +
-                    ",\t\t" + repetitionNumber + ",\t\t\t";
-            // Create the strinBuilder for adding the missing values and output on each line
-
-            // RowLoop
-            for (int i = 0; i < arrayLen; i++) {
-
-                //Column Loop
-                for (int j = 0; j < cycleLen; j++) {
-
-                    int arrayLength = arrayLengths[i];
-                    int cycleNumber = cycleNumbers[j];
-                    long runTime = results.data[i][j];
-
-                    String toAppend = (String) resultLine + " " + arrayLength + ",\t\t\t\t" + cycleNumber + "," +
-                            "\t\t\t\t" + runTime + ",\t\t\t";
-                    br.write(toAppend);
-                    br.newLine();
-                }
-            }
-
-            br.newLine();
-            br.close();
-
-/*            for (int i = 0; i <= rowNumbers; i++) {
-            }*/
-
-
-            // Close the connection to the file and finish off this result
-            br.close();
-
-        } catch (FileNotFoundException e) {
-//            Utility.logger.error("[linux]The folder results does not exists, and cannot create the folder", e);
-            e.printStackTrace(System.out);
-        } catch (IOException e2) {
-//            Utility.logger.error("Error in the BufferedWriter part of writeresults2CSV", e2);
-            e2.printStackTrace(System.out);
-        }
+//
+//        try {
+//
+//
+//        } catch (Exception e) {
+//
+//        }
+//
+//            // Create needed local data
+//
+//
+//
+//            // Create the results folder
+//            String fileDir = nameCPU + "_results";
+//            File fileDirObject = new File(fileDir);
+//            //ToDo: Test if folder exits and ensure it works on windows AND linux
+//            if (!fileDirObject.exists()) fileDirObject.mkdir();
+//
+//            // Create the file and open the connection
+//            String filename = fileDir + "/" + nameCPU + "_res_" + dataType + "_" +
+//                    operation + "_rep_" + repetitionNumber + ".csv";
+//
+//            // Open up the connection and create a string builder
+//            BufferedWriter br = new BufferedWriter(new FileWriter(filename));
+//            StringBuilder header = new StringBuilder();
+//
+//            // Create the header/first line
+//            header.append("name,\t\t DataType,\t isBoxed,\t operation,\t Repetition,\t ArrayLength\t " +
+//                    "CycleNumber,\t RunTime,\t");
+//            br.write(header.toString());
+//            br.newLine();
+//
+//            // Begin writing the actual data
+//            int arrayLengthNumbers = runSettings.getArrayLengths().length;
+//            int cycleLengthNumbers = runSettings.getCycleNumbers().length;
+//            int rowNumbers = arrayLengthNumbers * cycleLengthNumbers;
+//
+//
+//            //Start producing each line
+//            int[] arrayLengths = runSettings.getArrayLengths();
+//            int[] cycleNumbers = runSettings.getCycleNumbers();
+//            int arrayLen = arrayLengths.length;
+//            int cycleLen = cycleNumbers.length;
+//
+//            int rowIndex = 0, columnIndex = 0;
+//
+//            //StringBuilder rowLine = new StringBuilder();
+//            //rowLine.append(resultLine);
+//            // Missing values are ArrayLength CycleNumber and Runtime
+//            String resultLine = nameCPU + ",\t   " + dataType + ",\t   " + isBoxed + ",\t\t" + operation +
+//                    ",\t\t" + repetitionNumber + ",\t\t\t";
+//            // Create the strinBuilder for adding the missing values and output on each line
+//
+//            // RowLoop
+//            for (int i = 0; i < arrayLen; i++) {
+//
+//                //Column Loop
+//                for (int j = 0; j < cycleLen; j++) {
+//
+//                    int arrayLength = arrayLengths[i];
+//                    int cycleNumber = cycleNumbers[j];
+//                    long runTime = results.data[i][j];
+//
+//                    String toAppend = (String) resultLine + " " + arrayLength + ",\t\t\t\t" + cycleNumber + "," +
+//                            "\t\t\t\t" + runTime + ",\t\t\t";
+//                    br.write(toAppend);
+//                    br.newLine();
+//                }
+//            }
+//
+//            br.newLine();
+//            br.close();
+//
+///*            for (int i = 0; i <= rowNumbers; i++) {
+//            }*/
+//
+//
+//            // Close the connection to the file and finish off this result
+//            br.close();
+//
+//        } catch (FileNotFoundException e) {
+////            Utility.logger.error("[linux]The folder results does not exists, and cannot create the folder", e);
+//            e.printStackTrace(System.out);
+//        } catch (IOException e2) {
+////            Utility.logger.error("Error in the BufferedWriter part of writeresults2CSV", e2);
+//            e2.printStackTrace(System.out);
+//        }
 
     }
 }
