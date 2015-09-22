3d9cde93a13fb6b0e07a82493045296ccc67baf5
Slentzen Demian <slentzen@gmail.com>
2015-09-22 15:20:04 +0200

    Done commenting on Factory

--- src/main/java/org/hayabaya/loopers/LooperFactory.java
+++ src/main/java/org/hayabaya/loopers/LooperFactory.java
@@ -3,13 +3,25 @@ package org.hayabaya.loopers;
 import org.hayabaya.datarelated.Tpe;
 
 /**
- * Created by cain on 5/28/2015.
+ * Factory class that creates data type specific instances of the Loopers class.
+ * The class can return an instance of any of the 8 different datatypes available in Hayabaya.
+ * @see org.hayabaya.datarelated.Tpe
  */
 public class LooperFactory {
     private static LooperFactory instance = new LooperFactory();
     private LooperFactory(){}
     public static LooperFactory getinstance(){ return instance; }
 
+    /**
+     * Returns an instance of one of the Loopers_DATA_TYPE classes. To get an instance of a LoopersInt class use
+     * <pre>
+     *     LooperFactory looperFactory = LooperFactory.getinstance();
+     *     Loopers anIntInstance = looperFactory.createLooperInstance(Tpe.INT);
+     * </pre>
+     * @param type One of the datatypes from the Tpe enum class @see org.hayabaya.datarelated.Tpe
+     * @return A data type specific instance of Loopers sub-classes
+     * @throws IllegalArgumentException Default fallthrough if argument does not match any known data type
+     */
     public Loopers createLooperInstance(Tpe type) throws IllegalArgumentException{
         switch (type){
             case INT:
