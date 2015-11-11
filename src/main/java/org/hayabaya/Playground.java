8edc1eabf812de4bd7534b07205e79561ed70fc0
Slentzen Demian <slentzen@gmail.com>
2015-11-11 17:38:24 +0100

    readding the test classes to git

---
+++ src/main/java/org/hayabaya/Playground.java
@@ -0,0 +1,12 @@
+package org.hayabaya;
+
+/**
+ * Created by cain on 21/09/15.
+ */
+public class Playground {
+    public static void main(String[] args) {
+//        String wd = System.getProperty("user.dir");
+//        System.out.println("user.dir is" + wd);
+//        MainClass.main(new String[] {"i7", "small", "1"});
+    }
+}
