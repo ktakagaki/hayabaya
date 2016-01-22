a769f1eaed8c7186de42503e9ddbb0a9d51f3721
Slentzen Demian <slentzen@gmail.com>
2016-01-22 11:11:13 +0100

    Added Scala versions of the Java Enums Operation and Tpe DataType

--- src/main/scala/de/lin_magdeburg/hayabaya/datarelated/Operation.scala
+++ src/main/scala/de/lin_magdeburg/hayabaya/datarelated/Operation.scala
@@ -1,7 +1,17 @@
 package de.lin_magdeburg.hayabaya.datarelated
 
 /**
-  * Created by cain on 1/9/16.
+  * The operations that are available in Hayabaya
+  * <ul>
+  * <li><b>ADD:</b> Addition</li>
+  * <li><b>SUBTRACT:</b> Subtraction</li>
+  * <li><b>MULTIPLY:</b> Multiplication</li>
+  * <li><b>DIVIDE:</b> Division</li>
+  * <li><b>LESSTHAN:</b> \< a less than b comparison </li>
+  * <li><b>GREATERTHAN:</b> \> a greater than b comparison </li>
+  * <li><b>EQUALS:</b> \= equality comparison </li>
+  * <li><b>NOTEQUAL:</b> \!\= non equality comparison </li>
+  * </ul>
   */
 object Operation extends Enumeration {
   type Operation = Value
