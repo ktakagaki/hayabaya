9c9b58c74b2e2ece73196825b9cb1f3d1900f112
Slentzen Demian <slentzen@gmail.com>
2016-01-22 11:28:47 +0100

    Able to use the Scala enumeration objects

--- src/main/scala/de/lin_magdeburg/hayabaya/HayabayaMain.scala
+++ src/main/scala/de/lin_magdeburg/hayabaya/HayabayaMain.scala
@@ -1,29 +1,35 @@
 package de.lin_magdeburg.hayabaya
 
-import scala.util.Try
+import de.lin_magdeburg.hayabaya.datarelated._
 
 
 /**
   * Created by cain on 1/9/16.
   */
-object HayabayaMain /*extends App*/ {
+object HayabayaMain {
 
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-    Util.sessionLogger.info("Initializing hayabaya")
-    Util.sessionLogger.trace("This is a trace, not important")
-    Util.sessionLogger.error("Error, something bad has happened!")
-    Util.sessionLogger.warn("Warning, are you sure?")
+    Util.sessionLogger.debug("Starting Hayabaya Main Class")
+    println("[In Scala] Welcome to the Hayabaya Microbenchmark project")
 
-    try {
-      Util.throwError
+    val aTPE = Tpe.DOUBLE
+
+    println("The value of aTPE is: " + aTPE)
+
+  }
+}
+
+// ========== Example of using Logging ================== //
+/*Util.sessionLogger.trace("This is a trace, not important")
+Util.sessionLogger.error("Error, something bad has happened!")
+Util.sessionLogger.warn("Warning, are you sure?")
+Util.sessionLogger.info("Initializing hayabaya")*/
+
+
+// ========== Example of Throwing and Catching Exceptions ================== //
+/*    try {
+      Util.throwErrorForTesting
     } catch {
         case e: IllegalArgumentException => Util.sessionLogger.error("caught error", e)
         case _ => println("didn't recongize error")
-    }
-  }
-
-
-
-
-}
+    }*/
