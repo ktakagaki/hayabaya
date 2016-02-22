0e7243620720863d729705de7a47d34260bc12e0
Slentzen Demian <slentzen@gmail.com>
2016-02-22 14:19:07 +0100

    adding first benchmarker, integer

---
+++ src/main/scala/de/lin_magdeburg/hayabaya/benchmarkers/AbstractBenchmark.scala
@@ -0,0 +1,10 @@
+package de.lin_magdeburg.hayabaya.benchmarkers
+
+import de.lin_magdeburg.hayabaya.datarelated.Datatypes
+
+/**
+  * Created by admin on 2/22/2016.
+  */
+abstract class AbstractBenchmark(val datatype: Datatypes) extends BenchTrait {
+
+}
---
+++ src/main/scala/de/lin_magdeburg/hayabaya/benchmarkers/BenchMarkTrait.scala
@@ -0,0 +1,10 @@
+package de.lin_magdeburg.hayabaya.benchmarkers
+
+/**
+  * Created by admin on 2/22/2016.
+  */
+trait BenchMarkTrait {
+  def defaultMessage(): String = "BenchTrait message"
+  def datatypeMessage(): String
+
+}
