9c1a6f0c7ee96aa4b8d4ed46fa94bc2bf946324a
Slentzen Demian <slentzen@gmail.com>
2016-05-27 15:32:38 +0200

    Finally got system.process calls to work

--- playground/scalascript.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by cain on 27/05/16.
-  */
-
-case class Employee(name: String, office: String, role: String)
-
---
+++ src/main/scala/playground/RandomClass.scala
@@ -0,0 +1,13 @@
+package playground
+
+/**
+  * Created by cain on 27/05/16.
+  */
+class RandomClass {
+
+}
+
+
+object RunRandomClass extends App {
+
+}
