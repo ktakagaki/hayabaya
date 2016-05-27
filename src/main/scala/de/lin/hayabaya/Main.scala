a99e9704b73f269bef53678a22773c4b67524c20
Slentzen Demian <slentzen@gmail.com>
2016-05-27 18:58:42 +0200

    Autoformatted by scalariform

--- src/main/scala/de/lin/hayabaya/Main.scala
+++ src/main/scala/de/lin/hayabaya/Main.scala
@@ -4,8 +4,7 @@ package de.lin.hayabaya
  * Created by cain on 27/05/16.
  */
 case class MainConfig(error: Boolean, configfilepath: Option[String],
-  minarraylength: Option[Int]
-) {}
+  minarraylength: Option[Int]) {}
 
 class Main {
 
