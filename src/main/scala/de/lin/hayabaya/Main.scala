015f89d4555ed51dab4a6db716744813ba6c13d7
Slentzen Demian <slentzen@gmail.com>
2016-05-28 10:46:21 +0200

    formatting fucked up. Caused by intellij autoformat code, scalariform cannot solve the issue.
    Must manually fix it all

--- src/main/scala/de/lin/hayabaya/Main.scala
+++ src/main/scala/de/lin/hayabaya/Main.scala
@@ -5,40 +5,47 @@ package de.lin.hayabaya
  * paths, where to read any application.conf configuration file, number of repetitions, name of
  * CPU/system and so forth.
  */
-case class MainConfig(error: Boolean,
-  help: Option[String],
-  runsmalltest: Boolean,
-  cpuname: Option[String],
-  minarraysize: Option[Int],
-  maxarraysize: Option[Int],
-  arraystepsize: Option[Int],
-  pathtoconfigfile: Option[String])
-
+case class MainConfig(error: Boolean, help: Option[String], runsmalltest: Boolean, cpuname: Option[String], minarraysize: Option[Int], maxarraysize: Option[Int], arraystepsize: Option[Int], pathtoconfigfile: Option[String])
 
 /**
-  * Mainclass loading the CLI args, validating them, parsing them to other parts of code that
-  * then executes the jobs. Once completed Main will assemble the results and write them to disk.
-  */
+ * Mainclass loading the CLI args, validating them, parsing them to other parts of code that
+ * then executes the jobs. Once completed Main will assemble the results and write them to disk.
+ */
 object Main {
 
   /**
-    * Usage flags for Hayabaya, their -fFN and --flagFullName versions, and an explanation of
-    * what they flags are to be used for. If no flags are given, Hayabaya prints out the help
-    * dialog. For convenience that is a smalltest flag users can give to just run a quick and
-    * dirty small testsample.
-    */
+   * Usage flags for Hayabaya, their -fFN and --flagFullName versions, and an explanation of
+   * what they flags are to be used for. If no flags are given, Hayabaya prints out the help
+   * dialog. For convenience that is a smalltest flag users can give to just run a quick and
+   * dirty small testsample.
+   */
   private def usage() = {
-    println("\n \t\t ===== Welcome to Hayabaya ===== ")
-    println("Usage: Hayabaya [options] <source directory>")
-    println(" -h,   --help               Prints out this help message")
-    println(" -t,   --test               Runs a default small test sample for testing purposes")
-    println(" -c,   --config FILE        Configuration file (if NONE, defaults to internal default")
-    println(" -n,   --name STRING        Name of the CPU and or System Hayabaya is profiling")
-    println(" -min, --minArraySize INT   The starting array size to profile")
-    println(" -max, --maxArraySize INT   The end size of the array to profile")
-    println(" -s,   --stepSize INT       The step size / stride to be used in the [min,max] array")
+    println("\n \t\t ===== Welcome to Hayabaya ===== ") println ("Usage: Hayabaya -options [argument type]") println ("============================================") println ("-h,   --help               Prints this help message") println ("-t,   --test               Runs a predefined small test sample") println ("-c,   --config [FILE]      Configuration file (if none given, default is used)") println ("-n,   --name [STRING]      Name of the CPU and/or Computer System") println ("-min, --minArraySize [INT] The minimum array size to profile") println ("-max, --maxArraySize [INT] The maximum array size to profile") println ("-s,   --stepSize [INT]     The stepsize/stride to be used in the [min,max] array") println ("============================================")
   }
 
+  /**
+   * Always be case insensitive when evaluating strings for "true"
+   *
+   * @param s
+   * @return true if string matches "true", "True", "TRUE" and so forth
+   */
+  private def isTrue(s: String): Boolean = {
+    "true".equalsIgnoreCase(s)
+  }
+
+  def parseArgs(args: Array[String]): MainConfig = {
+    var config = MainConfig(false) // initialize error to false
+    var i = 0
+
+    while (i < args.length) {
+      if (args(i).startsWith("-") && i < (args.length - 1)) {
+        args(i) match {
+          case ("-h" | "--help") => usage()
+
+        }
+      }
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     usage()
