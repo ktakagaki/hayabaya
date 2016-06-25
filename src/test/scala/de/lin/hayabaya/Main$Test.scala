8da676042682a58d2c85774c755439499414b0af
Slentzen Demian <evenprime@tutanota.com>
2016-06-25 08:16:14 +0200

    WTF?

--- src/test/scala/de/lin/hayabaya/Main$Test.scala
+++ src/test/scala/de/lin/hayabaya/Main$Test.scala
@@ -1,14 +1,30 @@
 package de.lin.hayabaya
 
 import org.scalatest.{FlatSpec, FunSuite}
+import org.scalatest.BeforeAndAfter
+
+import de.lin.hayabaya._
 
 /**
   * Created by cain on 23/06/16.
   */
-class Main$Test extends FlatSpec {
+class Main$Test extends FunSuite with BeforeAndAfter {
 
-  test("testMain") {
+  var mainConfig: MainConfig = _
+  val usageMessage: String = Main.usage;
 
+  before {
+    mainConfig = MainConfig()
+  }
+
+  test("When calling hayabaya without any args, it should display usage") {
+
+    assert(Main.usage === usageMessage, "The usage message is not correct or not printed")
+  }
+
+  test("When calling with illegal args, or wrong number of args usage is displayed") {
+
+    assert(Main.usage === usageMessage, "The usage message is not correct or not printed")
   }
 
 }
