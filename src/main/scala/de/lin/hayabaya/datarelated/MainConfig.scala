f40c9c88b61a196ac6b459e9e2b73abc38f007ab
Slentzen Demian <evenprime@tutanota.com>
2016-06-25 16:19:18 +0200

    currently writing unit tests for MainConfig as modifying MainConfig has proven more difficult than anticipated

--- src/main/scala/de/lin/hayabaya/datarelated/MainConfig.scala
+++ src/main/scala/de/lin/hayabaya/datarelated/MainConfig.scala
@@ -1,7 +1,5 @@
 package de.lin.hayabaya.datarelated
 
-import com.typesafe.scalalogging.LazyLogging
-
 /**
  * Main configuration of the project at runtime.
  * contains values for arraylengths, CPU/system name and so forth.
@@ -11,7 +9,4 @@ case class MainConfig (
   systemName: String = "i73770k",
   minarraysize: Int = 2024,
   maxarraysize: Int = 4048,
-  arraystepsize: Int = 8 ) {
-
-  override def apply(runTest: Boolean, ): MainConfig = new MainConfig(runtest, systemName, minarraysize, maxarraysize, arraystepsize)
-}
+  arraystepsize: Int = 8 )
---
+++ src/main/scala/testMainConfig.sc
@@ -0,0 +1,7 @@
+import de.lin.hayabaya.datarelated._
+import de.lin.hayabaya.util._
+
+val conf: MainConfig = {
+  MainConfig()
+}
+println(conf)
